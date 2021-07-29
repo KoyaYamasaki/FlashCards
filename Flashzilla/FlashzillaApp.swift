a286a247f941b80c0a9459354d883785d9a63a88
Yamasaki Kouya <koya2170@gmail.com>
2021-07-29 16:46:55 +0900

    First commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by 山崎宏哉 on 2021/07/27.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Flashzilla
+
+## Use :
+- CoreHaptics
+- Accessibility
+  1. DifferentiateWithoutColor
+  2. VoiceOver Accessibility
